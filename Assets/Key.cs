f5399bddbdae72c8be0465d50a2497101001ca68
Sliemon-Floof <89981972+Sliemon-Floof@users.noreply.github.com>
2022-01-21 17:29:13 +0100

    some stuff

--- Assets/Key.cs
+++ Assets/Key.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class Key : MonoBehaviour
 {
+    [SerializeField]
+    private KeyCode activateKey;
+    private bool isInBox;
+    private SpriteRenderer sprite;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        sprite = GetComponent<SpriteRenderer>();   
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(activateKey) && isInBox)
+        {
+            print("yaas");
+            sprite.enabled = false;
+           
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        isInBox = true;
+
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        isInBox = false;
     }
 }
+

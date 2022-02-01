f55207f9d6515feb96b89a362ec9d4765a162a04
Sliemon-Floof <89981972+Sliemon-Floof@users.noreply.github.com>
2022-02-01 14:37:51 +0100

    asdfghjklöluytrews

--- Assets/AutoRoomSwitch.cs
+++ Assets/AutoRoomSwitch.cs
@@ -8,43 +8,36 @@ public class AutoRoomSwitch : MonoBehaviour
     [SerializeField]
     private string sceneToSwitchTo;
     private string currentScene;
-    [SerializeField]
-    private KeyCode activateKey;
     private GameObject goblin;
     [SerializeField]
-    private Vector2 playerPos;
-    private float cooldown;
+    private Vector3 playerPos;
 
     private bool isInBox;
     // Start is called before the first frame update
     void Start()
     {
         currentScene = gameObject.scene.name;
-        goblin = FindObjectOfType<GameObject>(tag == "Player");
     }
-   
+
+
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        print("yaas");
         SceneManager.UnloadSceneAsync(currentScene);
         SceneManager.LoadScene(sceneToSwitchTo, LoadSceneMode.Additive);
+        goblin = GameObject.Find("green_hair_girl_spritesheet_0");
         goblin.transform.position = playerPos;
 
     }
 
+    /*
+
+                print("yaas");
+                SceneManager.LoadScene("TransitionRoom", LoadSceneMode.Additive);
+                SceneManager.UnloadSceneAsync(currentScene);
+                SceneManager.LoadScene(sceneToSwitchTo, LoadSceneMode.Additive);
+                SceneManager.UnloadSceneAsync("TransitionRoom");
+    */
 }
-
-/*
-
-            print("yaas");
-            SceneManager.LoadScene("TransitionRoom", LoadSceneMode.Additive);
-            SceneManager.UnloadSceneAsync(currentScene);
-            SceneManager.LoadScene(sceneToSwitchTo, LoadSceneMode.Additive);
-            SceneManager.UnloadSceneAsync("TransitionRoom");
-*/

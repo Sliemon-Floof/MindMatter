eb068ae051b9603870fa9864ad8994ab76ca9cde
Sliemon-Floof <89981972+Sliemon-Floof@users.noreply.github.com>
2022-02-08 13:33:02 +0100

    qwertyuiop

---
+++ Assets/Scenes/Allways_Loaded/Bools.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+ __          __  _                            _______      ____              _     
+ \ \        / / | |                          |__   __|    |  _ \            | |    
+  \ \  /\  / /__| | ___ ___  _ __ ___   ___     | | ___   | |_) | ___   ___ | |___ 
+   \ \/  \/ / _ \ |/ __/ _ \| '_ ` _ \ / _ \    | |/ _ \  |  _ < / _ \ / _ \| / __|
+    \  /\  /  __/ | (_| (_) | | | | | |  __/    | | (_) | | |_) | (_) | (_) | \__ \
+     \/  \/ \___|_|\___\___/|_| |_| |_|\___|    |_|\___/  |____/ \___/ \___/|_|___/
+                                                                                   
+*/                                                                                   
+
+[System.Serializable]
+public class Bools : MonoBehaviour
+{
+    private bool hasLibraryKey;
+}

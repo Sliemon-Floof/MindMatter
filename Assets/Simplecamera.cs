a1b647d5c44556050eda97dd0d7486233d7c0c9d
Sliemon-Floof <89981972+Sliemon-Floof@users.noreply.github.com>
2022-02-16 21:39:03 +0100

    qwerty

--- Assets/Simplecamera.cs
+++ Assets/Simplecamera.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class Simplecamera : MonoBehaviour
 {
+    private GameObject goblin;
     // Start is called before the first frame update
     void Start()
     {
-        
+        goblin = goblin = GameObject.Find("green_hair_girl_spritesheet_0");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        gameObject.transform.position -= new Vector3(0.05f*(transform.position.x - goblin.transform.position.x),0,0);
     }
 }

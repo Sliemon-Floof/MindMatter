8316691023e93e1bded40a4a0019143c59c373f0
ueaex <maria.yildiz@elev.ga.lbs.se>
2022-02-17 13:25:00 +0100

    PAUS MENY KLAR

--- Assets/PauseMenu.cs
+++ Assets/PauseMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -11,6 +12,7 @@ public class PauseMenu : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+        //Om man klickar escape så öppnas paus menyn -Maria
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
@@ -25,19 +27,28 @@ public class PauseMenu : MonoBehaviour
         }
     }
 
-    void Resume()
+    public void Resume()
+        //När man klickar på resume så är time speed som vanligt -Maria
     {
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
         GameIsPaused = false;
     }
     void Pause()
+        //När spelet är pausad så är time speeden på 0 -Maria
     {
 
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         GameIsPaused = true;
         Debug.Log("Paused");
+    }
+
+    public void Quit()
+        //Stänger spel när man klickar på quit knappen -Maria
+    {
+        Debug.Log("game closes");
+        Application.Quit();
     }
 }
     
